3387ca2ebcb6a4da0c79133188b53e962466250e
Ryan Awad <ryanawad@yahoo.com>
2024-04-20 16:52:25 -0400

    Turbo is not stable which does not make it production ready. We will be developing the tracker app in a monolithic next application

--- apps/backend/src/routes/ping.ts
+++
@@ -1,9 +0,0 @@
-import express, { Router, Request, Response } from "express";
-
-const router: Router = express.Router();
-
-router.get('/ping', (req: Request, res: Response) => {
-  res.status(200).send('Pong!');
-});
-
-export default router;
