4352034ff4942baf31236146b62f5d656118a7cf
Ryan Awad <ryanawad@yahoo.com>
2024-04-20 20:01:06 -0400

    Using Zod to validate request bodies

--- pages/api/dbTests/createProduct.ts
+++ pages/api/dbTests/createProduct.ts
@@ -1,9 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Product } from '../../../server/objects'
+import z from 'zod';
  
 type ResponseData = {
   product: string
 }
+
+const bodySchema = z.object({
+  productName: z.string(),
+  productPrice: z.number()
+});
+
+// TODO: create middleware to validate all request bodies using Zod.
  
 export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
   if (req.method === 'POST') {
