26b4875e506859119779ed2697a0d3d4d03aa3a8
Ryan Awad <ryanawad@yahoo.com>
2024-05-08 16:26:57 -0400

    Removed JWT related env vars

--- env.ts
+++ env.ts
@@ -7,12 +7,8 @@ const envSchema = z.object({
   POSTGRES_USER: z.string().min(1).optional(),
   POSTGRES_PASSWORD: z.string().min(1).optional(),
   POSTGRES_ADDR: z.string().min(1).optional(),
-
-  JWT_SECRET: z.string().min(64),
 })
 
 const ENV = envSchema.parse(process.env);
-if (ENV.NODE_ENV === 'development')
-  console.log(`Generated JWT_SECRET: ${ENV.JWT_SECRET}`);
 
 export default ENV;
