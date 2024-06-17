8a16a9fe49dfa3632471529626a2e7f9f430e909
Aashna-Verma <aashna.verma@outlook.com>
2024-06-16 22:56:26 -0400

    docs: FumaDocs up, no search yet, bullet points weird

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,17 @@
 import type { Config } from 'tailwindcss'
+import { createPreset } from 'fumadocs-ui/tailwind-plugin'
 
 const config: Config = {
+  darkMode: 'class',
+  presets: [createPreset({
+    preset: 'dusk',
+  })],
   content: [
+    './node_modules/fumadocs-ui/dist/**/*.js',
+
+    './content/docs/*.mdx',
+    './mdx-components.tsx',
+
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
